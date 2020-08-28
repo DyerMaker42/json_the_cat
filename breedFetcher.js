7c92fc5f4eeb4f53ee0705ab256bdc06f399be61
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-08-28 01:14:21 +0000

    mutated data into object, isolated description

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,11 +5,11 @@ const breed = 'sibe';
 const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
 request.get(url, (error, response, body) => {
-  
+  const data = JSON.parse(body)
   if (error) {
     console.log('log the error', error);
     return;
   }
-  console.log(body);
+  console.log(data[0]["description"], typeof data);
 
 });
